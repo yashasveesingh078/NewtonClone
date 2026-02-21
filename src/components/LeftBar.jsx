e38964a8b3aa7675d85e1363fe7031444484ccdc
yashasveesingh078 <yashasveesingh078@gmail.com>
2026-02-21 11:04:39 +0530

    leftbar

--- src/components/LeftBar.jsx
+++ src/components/LeftBar.jsx
@@ -1,11 +1,101 @@
-import React from 'react'
+import {
+  Home,
+  TrendingUp,
+  GitBranch,
+  Users,
+  Calendar,
+  Code,
+  Trophy,
+  FileText,
+  Shield,
+  HelpCircle,
+} from "lucide-react";
 
 function LeftBar() {
   return (
-    <div>
-      
+    <div className="w-70 h-full border-r border-gray-200 px-6 py-6">
+
+
+      <div className="mb-6">
+        <h2 className="text-md font-bold">S2'25 CS+AI RU</h2>
+        <span className="inline-block mt-2 bg-green-200 text-green-800 text-xs px-3 py-1 rounded-md font-medium">
+          Enrolled
+        </span>
+      </div>
+      <div  className=" border-b border-gray-100 m-4"></div>
+    
+
+      <div className="bg-blue-100 text-blue-600 flex items-center gap-4 px-4 py-5 rounded-xl mb-6 cursor-pointer h-12 w-50 hover:bg-gray-200">
+        <Home size={22} />
+        <span className="text-sm font-medium ">Home</span>
+      </div>
+
+
+      <div className="mb-6">
+        <h3 className="text-s font-semibold mb-3 pl-2">Subjects</h3>
+
+       <div className="border-2 border-gray-200 rounded-2xl p-4 space-y-2">        
+          <p className="text-sm cursor-pointer hover:bg-gray-200 rounded-xl h-9 flex items-center px-2">DSA - A</p>
+          <p className="text-sm cursor-pointer hover:bg-gray-200 rounded-xl h-9 flex items-center px-2">DSA Lab 1 - A</p>
+          <p className="text-sm cursor-pointer hover:bg-gray-200 rounded-xl h-9 flex items-center px-2">Maths II - A</p>
+          <p className="text-sm cursor-pointer hover:bg-gray-200 rounded-xl h-9 flex items-center px-2">Maths II Tut 1 - A</p>
+          <p className="text-sm cursor-pointer hover:bg-gray-200 rounded-xl h-9 flex items-center px-2">WAP - A</p>
+          <p className="text-sm cursor-pointer hover:bg-gray-200 rounded-xl h-9 flex items-center px-2">WAP Lab 1 - A</p>
+
+        </div>
+      </div>
+
+      <div className="space-y-8 text-sm">
+
+        <div className="flex items-center gap-4 cursor-pointer hover:bg-gray-200 rounded-xl h-9 px-2">
+          <TrendingUp size={20} />
+          <span>Scorecard</span>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer hover:bg-gray-200 rounded-xl h-9 px-2">
+          <GitBranch size={20} />
+          <span>My Timeline</span>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer hover:bg-gray-200 rounded-xl h-9 px-2">
+          <Users size={22} />
+          <span>Expert Sessions</span>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer hover:bg-gray-200 rounded-xl h-9 px-2">
+          <Calendar size={22} />
+          <span>Calendar</span>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer hover:bg-gray-200 rounded-xl h-9 px-2">
+          <Code size={22} />
+          <span>Arena</span>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer hover:bg-gray-200 rounded-xl h-9 px-2">
+          <Trophy size={22} />
+          <span>Leaderboard</span>
+        </div>
+
+        <div className="flex items-center gap-4 cursor-pointer hover:bg-gray-200 rounded-xl h-9 px-2">
+          <FileText size={22} />
+          <span>Question of the Day</span>
+        </div>
+
+      </div>
+
+      <div className="mt-6 bg-green-100 px-5 py-3 rounded-2xl flex items-center gap-4 cursor-pointer text-green-800 font-sm hover:bg-gray-200">
+        <Shield size={22} />
+        <span className="text-sm">Share a Concern</span>
+      </div>
+
+      <div className="mt-6 flex items-center gap-4 cursor-pointer text-sm hover:bg-gray-200 rounded-xl h-9 px-2">
+        <HelpCircle size={22} />
+        <span>Help & Support</span>
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default LeftBar
+export default LeftBar;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
+import { ChevronDown } from 'lucide-react';
+import { Users } from 'lucide-react';
+import { Bell } from 'lucide-react';
+import { CircleUserRound } from 'lucide-react';
 function Navbar() {
   return (
     <>
-      <div className="flex gap-2 items-center p-2 h-15 w-full border-b border-gray-200">
+      <div className="flex gap-2 items-center p-2 h-15 w-full border-b border-gray-200"><button>
         <div className="flex gap-2 p-2 bg-white text-sm rounded-xl border border-gray-200">
           <img
             src="https://d3dyfaf3iutrxo.cloudfront.net/general/upload/bbe791d38e9a4bc188b07d81691a6372.svg"
             className="h-5"
           />
-          NST'25 CS+AI RU
+          NST'25 CS+AI RU<ChevronDown/>
         </div>
-        <div className="gap-2 pl-5 pr-5 pt-1 pb-1 bg-amber-100 text-sm rounded-3xl font-bold border border-gray-200">
+        </button>
+        <div className="bg-amber-100 p-1 rounded-3xl m-2 text-sm pl-3 cursor-pointer  h-11 flex items-center gap-x-6 border-gray-200 border font-semibold">
           Download latest version of Athena.
           <button className=" bg-amber-600 pt-1.5 pb-1.5 pl-2 pr-2 text-sm rounded-2xl text-white font-bold">
             Download
@@ -23,9 +28,13 @@ function Navbar() {
               <p>10,768</p>
             </div>
           </div>
-          <div>
-            
+          <div className='border-l border-gray-200 pl-6 flex items-center gap-6 ml-6'>
+          <Users />
+          <Bell />
+          <button className='flex hover:bg-gray-200 rounded-2xl h-10 w-15 items-center justify-center cursor-pointer'><CircleUserRound /> <ChevronDown size={20}/></button>
+
           </div>
+
         </div>
       </div>
     </>
